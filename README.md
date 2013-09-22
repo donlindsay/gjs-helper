d82af0bc3ef1fd439d9ceec875be52f03c76acce
Donald Lindsay <don.lindsay@gmail.com>
2013-09-22 14:48:40 -0700

    reorganized tutorials, added another, updated README.md

--- README.md
+++ README.md
@@ -3,10 +3,12 @@ gjs-mode.el
 
 I like the idea of using Gnome's GJS javascript shell for
 coding. js2-mode and js-comint provide a solid foundation for
-developing a mode for using gjs with emacs.
+developing a mode for using gjs with emacs. My main concept so far is
+to lift the better ideas out of js-comint.el and add features like app
+templates and maybe even some of the Looking Glass stuff.
 
-Currently, gjs-mode.el depends on both js2-mode and js-comint which
-can be found at:
+Dependencies
+------------
 
 js2-mode:   https://github.com/mooz/js2-mode?source=c
             note: js2-mode exists in the melpa emacs package repository 
@@ -14,20 +16,29 @@ js2-mode:   https://github.com/mooz/js2-mode?source=c
 
 js-comint:  http://sourceforge.net/projects/js-comint-el/?source=directory
 
-GJS documentation can be found here:
-https://wiki.gnome.org/Gjs
+And, of course, GJS. GJS exists in the Debian and Ubuntu repositories
+as 'gjs' and can be installed with: 'sudo apt-get install gjs' Arch
+users can find GJS in the 'Extra' repository as 'gjs'. If this doesn't
+cover it for you, check your distribution's repositories, or perhaps
+you can build GJS from source.
 
-There is a GJS tutorial which I will include in the /hellognome dir:
+GJS Documentation and Tutorials
+-------------------------------
+
+GJS documentation can be found here: https://wiki.gnome.org/Gjs
+
+There are some GJS tutorial materials:
 https://developer.gnome.org/gnome-devel-demos/unstable/hellognome.js.html.en
 
-GJS exists in the Debian and Ubuntu repositories as 'gjs' and can be
-installed with: 'sudo apt-get install gjs'
+Some of these materials can be found in the /gnome-tutorial directory.
 
-Arch users can find GJS in the 'Extra' repository as 'gjs'.
+Thanks
+------
 
-My main concept so far is to lift the better ideas out of js-comint.el
-and add features like app templates and maybe even some of the Looking
-Glass stuff.
+I grateful for the excellent work done by the authors and maintainers
+of js2-mode, js-comint and of course the Gnome development community
+for their provision, and of course, the Emacs development community
+without whom this project would not exist.
 
 This is my first try at an Emacs mode. Anyone interested in contributing ideas 
 or code is welcome. Github facilitates this, or I can sometimes be found
---
+++ gnome-tutorials/02_welcome_to_the_grid/welcome_to_the_grid_1.js
@@ -0,0 +1,191 @@
+// welcome_to_the_grid_1.js 
+
+// This tutorial involves two parts. The first part,
+// welcome_to_the_grid_1.js, demonstrates the native gtk
+// application. The second part, welcome_to_the_grid_2.js, details the
+// code specific to creating the grid.
+
+// From the GJS tutorial located at:
+// https://developer.gnome.org/gnome-devel-demos/ 
+
+// Welcome to the Grid
+
+// This tutorial will show you how to create basic widgets, or parts
+// of the GNOME user interface, like Images and Labels. You'll then
+// learn how to arrange them all in a Grid, which lets you put widgets
+// exactly where you want them.
+
+// Have you taken the first tutorial in this series already? You'll
+// want to do so before continuing.
+
+// Going native
+
+// In the last tutorial, we created what was basically a GNOME window
+// frame for a web app. All the GNOME-specific code we needed to learn
+// revolved around putting the WebView -- the widget containing our
+// application -- into an ApplicationWindow, and telling it to
+// display. The application itself was written in HTML and JavaScript,
+// just like most pages on the web.
+
+// This time, we're going to use only native GNOME widgets. A widget
+// is just a thing, like a checkbox or picture, and GNOME has a wide
+// variety of them to choose from. We call them "native" widgets to
+// distinguish them from things like the button and header in the web
+// app we wrote. Because instead of using web code, these are going to
+// be 100 percent GNOME, using GTK+.
+
+// GTK+ stands for "GIMP Toolkit". It's like a toolbox of widgets that
+// you can reach into, while building your applications. It was
+// originally written for the GIMP, which is a free software image
+// editor.
+
+// Setting up our application
+
+// Before we dig out any widgets from the GTK+ toolbox, we first need
+// to write the basic boilerplate code that our application requires.
+
+#!/usr/bin/gjs
+
+const Gtk = imports.gi.Gtk;
+const Lang = imports.lang;
+
+// This part always goes at the start of your code. Depending on what
+// you'll be doing with it, you may want to declare more imports
+// here. What we're writing today is pretty basic, so these are all we
+// need; Gtk for the widgets, and Lang so we can use Lang.bind to
+// connect our application's activate and startup signals to the
+// requisite functions.
+
+// Speaking of which:
+
+const WelcomeToTheGrid = new Lang.Class({
+    Name: 'Welcome to the Grid',
+
+    // Create the application itself
+    _init: function() {
+        this.application = new Gtk.Application();
+
+        // Connect 'activate' and 'startup' signals to the callback functions
+        this.application.connect('activate', Lang.bind(this, this._onActivate));
+        this.application.connect('startup', Lang.bind(this, this._onStartup));
+    },
+
+    // Callback function for 'activate' signal presents windows when active
+    _onActivate: function() {
+        this._window.present();
+    },
+
+    // Callback function for 'startup' signal builds the UI
+    _onStartup: function() {
+        this._buildUI ();
+    },
+
+// This is the start of the application itself, and the _init function
+// which creates it. It tells _buildUI to create an ApplicationWindow,
+// which we're going to call _window, and it tells our window to
+// present itself whenever needed.
+
+// This part, again, is pretty much copy-and-paste, but you always
+// want to give your application a unique name.
+
+    // Build the application's UI
+    _buildUI: function() {
+
+        // Create the application window
+        this._window = new Gtk.ApplicationWindow({
+            application: this.application,
+            window_position: Gtk.WindowPosition.CENTER,
+            border_width: 10,
+            title: "Welcome to the Grid"});
+
+// Finally, we start off the _buildUI function by creating a new
+// ApplicationWindow. We specify that it goes with this application,
+// that it should appear in the center of the screen, and that there
+// should be at least 10 pixels between the outside edge and any
+// widgets inside of it. We also give it a title, which will appear at
+// the top of the window.
+
+// Reaching into the GTK+ toolbox
+
+// What widgets should we use? Well, let's say we want to write an
+// application that looks like this:
+
+// ./02_jsgrid_01.png
+
+// We're going to need, at the very least, a picture and a text label
+// to go with it. Let's start with the picture:
+
+        // Create an image
+        this._image = new Gtk.Image ({ file: "gnome-image.png" });
+
+// You can download the image file used in this example here. 
+
+// ./gnome-image.png
+
+// Be sure to put it in the same directory as the code you're writing.
+
+        // Create a label
+        this._label = new Gtk.Label ({ label: "Welcome to GNOME, too!" });
+
+// That code adds in the label beneath. You can see how we create
+// widgets, here; each one is a part of Gtk, and we can give it
+// properties that customize how it behaves. In this case, we set the
+// Image's file property to be the filename of the picture we want,
+// and the Label's label property to be the sentence that we want
+// beneath the picture.
+
+// Yes, it sounds redundant for a Label to have a label property, but
+// it's not. Other widgets that contain text have a label property, so
+// it's consistent for the Label widget to have one too.
+
+// We can't just add these widgets to our window in order, though, the
+// same way HTML elements appear in the order you write them. That's
+// because an ApplicationWindow can only contain one widget.
+
+// How do we get around that? By making that one widget a container
+// widget, which can hold more than one widget and organize them
+// inside it. Behold: The Grid.
+
+        // Create the Grid
+        this._grid = new Gtk.Grid ();
+
+// We're not giving it any properties yet. Those will come later, as
+// we learn how to use the Grid's powers. First, let's attach the
+// Image and Label we made to our Grid.
+
+        // Attach the image and label to the grid
+        this._grid.attach (this._image, 0, 0, 1, 1);
+        this._grid.attach (this._label, 0, 1, 1, 1);
+
+// This code looks awfully complicated, but it's not. Here's what
+// those numbers mean:
+
+// The first number is what left-to-right position to put things in,
+// starting from 0. Any widget that uses a 0 here goes all the way to
+// the left.
+
+// The second number is what top-to-botton position to put a given
+// widget in, starting from 0. The Label goes beneath the Image, so we
+// give the Image a 0 and the Label a 1 here.
+
+// The third and fourth numbers are how many columns and rows a widget
+// should take up. We'll see how these work in a minute.
+
+        // Add the grid to the window
+        this._window.add (this._grid);
+
+        // Show the window and all child widgets
+        this._window.show_all();
+    }
+
+});
+
+// Run the application
+let app = new WelcomeToTheGrid ();
+app.application.run (ARGV);
+
+// Now that we've created the Grid and attached all our widgets to it,
+// we add it to the window and tell the window to show itself, as the
+// last part of the _buildUI function. As always, to finish up we
+// create a new instance of the application's class and tell it to
+// run.
